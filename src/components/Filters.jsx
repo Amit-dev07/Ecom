f8bb2bb76391514edd32d6b7a9163727a46cc1d1
AMIT-BHAGAT <sbhagatamit@gmail.com>
2026-04-19 18:04:13 +0530

    added product and filter

---
+++ src/components/Filters.jsx
@@ -0,0 +1,10 @@
+const Filters = () => {
+  return (
+    <div className="filters-container">
+      <h3>Filters</h3>
+      {/* Add your filter components here */}
+    </div>
+  );
+};
+
+export default Filters;
