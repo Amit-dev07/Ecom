b2b074097f5e84e345b321975b1c82b28a85bce3
AMIT-BHAGAT <sbhagatamit@gmail.com>
2026-04-27 21:08:31 +0530

    contact page completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import './App.css'
-import Products from './components/Products'
+import React, { useState } from 'react'
+import Products from './components/products/Products'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './components/home/Home'
+import { Toaster } from 'react-hot-toast'
+import Navbar from './components/shared/Navbar'
+import About from './components/About'
+import Contact from './components/Contact'
 
 function App() {
 
   return (
-    <Products/>
+    <React.Fragment>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={ <Home />}/>
+          <Route path='/products' element={ <Products />}/>
+          <Route path='/about' element={ <About />}/>
+          <Route path='/contact' element={ <Contact />}/>
+        </Routes>
+      </Router>
+      <Toaster position='bottom-center'/>
+    </React.Fragment>
   )
 }
 
--- src/components/shared/ProductCard.jsx
+++ src/components/shared/ProductCard.jsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
 import { FaShoppingCart } from "react-icons/fa";
 import ProductViewModal from "./ProductViewModal";
+import truncateText from "../../utils/truncateText";
+import { useDispatch } from "react-redux";
+//import { addToCart } from "../../store/actions";
+import toast from "react-hot-toast";
+
 const ProductCard = ({
         productId,
         productName,
@@ -12,20 +17,25 @@ const ProductCard = ({
         specialPrice,
         about = false,
 }) => {
-
     const [openProductViewModal, setOpenProductViewModal] = useState(false);
     const btnLoader = false;
     const [selectedViewProduct, setSelectedViewProduct] = useState("");
     const isAvailable = quantity && Number(quantity) > 0;
+    const dispatch = useDispatch();
 
     const handleProductView = (product) => {
-        setSelectedViewProduct(product);
-        setOpenProductViewModal(true);
-    }
-  return (
+        if (!about) {
+            setSelectedViewProduct(product);
+            setOpenProductViewModal(true);
+        }
+    };
 
+    // const addToCartHandler = (cartItems) => {
+    //     dispatch(addToCart(cartItems, 1, toast));
+    // };
 
-    <div className="border rounded-lg shadow-xl overflow-hidden transition-shadow duration-300"> 
+    return (
+        <div className="border rounded-lg shadow-xl overflow-hidden transition-shadow duration-300">
             <div onClick={() => {
                 handleProductView({
                     id: productId,
@@ -38,7 +48,7 @@ const ProductCard = ({
                     specialPrice,
                 })
             }} 
-                className="w-full overflow-hidden aspect-3/2">
+                    className="w-full overflow-hidden aspect-3/2">
                 <img 
                 className="w-full h-full cursor-pointer transition-transform duration-300 transform hover:scale-105"
                 src={image}
@@ -46,8 +56,8 @@ const ProductCard = ({
                 </img>
             </div>
             <div className="p-4">
-                <h2 onClick = {() => {
-                    handleProductView({
+                <h2 onClick={() => {
+                handleProductView({
                     id: productId,
                     productName,
                     image,
@@ -57,19 +67,20 @@ const ProductCard = ({
                     discount,
                     specialPrice,
                 })
-                }} 
-                  className="text-lg font-semibold mb-2 cursor-pointer">
-                  {productName}
+            }}
+                    className="text-lg font-semibold mb-2 cursor-pointer">
+                    {truncateText(productName, 50)}
                 </h2>
-
+                
                 <div className="min-h-20 max-h-20">
                     <p className="text-gray-600 text-sm">
-                        {description}
+                        {truncateText(description, 80)}
                     </p>
                 </div>
 
+            { !about && (
                 <div className="flex items-center justify-between">
-                  {specialPrice ? (
+                {specialPrice ? (
                     <div className="flex flex-col">
                         <span className="text-gray-400 line-through">
                             ${Number(price).toFixed(2)}
@@ -78,33 +89,41 @@ const ProductCard = ({
                             ${Number(specialPrice).toFixed(2)}
                         </span>
                     </div>
-                  ) : (
+                ) : (
                     <span className="text-xl font-bold text-slate-700">
                         {"  "}
                         ${Number(price).toFixed(2)}
                     </span>
-                  )}
+                )}
 
-                <button
+                {/* <button
                     disabled={!isAvailable || btnLoader}
-                    onClick={() => {
-                    }}
+                    onClick={() => addToCartHandler({
+                        image,
+                        productName,
+                        description,
+                        specialPrice,
+                        price,
+                        productId,
+                        quantity,
+                    })}
                     className={`bg-blue-500 ${isAvailable ? "opacity-100 hover:bg-blue-600" : "opacity-70"}
                         text-white py-2 px-3 rounded-lg items-center transition-colors duration-300 w-36 flex justify-center`}>
                     <FaShoppingCart className="mr-2"/>
                     {isAvailable ? "Add to Cart" : "Stock Out"}
-                </button>
+                </button> */}
                 </div>
-
-              </div>
-
+            )}
+                
+            </div>
             <ProductViewModal 
                 open={openProductViewModal}
                 setOpen={setOpenProductViewModal}
                 product={selectedViewProduct}
                 isAvailable={isAvailable}
-            />                  
-    </div>
-)
+            />
+        </div>
+    )
 }
+
 export default ProductCard;
